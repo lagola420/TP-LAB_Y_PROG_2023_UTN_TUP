35dba7c79c77fb7606aecddef3fc1e354a8893ae
lagola420 <118774155+lagola420@users.noreply.github.com>
2023-09-10 16:29:17 -0300

    Update Sentencias DDL.sql

--- SQL/Sentencias DDL.sql
+++ SQL/Sentencias DDL.sql
@@ -30,7 +30,11 @@ CREATE TABLE Formas_Pago (
     CONSTRAINT Formas_Pago_pk PRIMARY KEY  (id_forma_pago)
 	)
 
-
+CREATE TABLE Formas_envio (
+    id_forma_envio int identity (1,1) NOT NULL,
+    forma_envio varchar (80) NOT NULL,
+    CONSTRAINT pk_Formas_envio PRIMARY KEY  (id_forma_envio)
+	)
 
 CREATE TABLE Tipos_Clientes(
     id_tipo_cliente int identity (1,1) NOT NULL,
@@ -44,14 +48,23 @@ CREATE TABLE Provincias (
     CONSTRAINT Provincias_pk PRIMARY KEY  (Id_provincia)
 )
 
+CREATE TABLE Tipos_Vehiculos (
+    id_tipoV int identity (1,1) NOT NULL,
+    nombre_tipo varchar (50) NOT NULL,
+    CONSTRAINT Tipos_Vehiculos_pk PRIMARY KEY (id_tipoV)
+);
+
+-- Creación de la tabla Vehiculos_Terminados
 CREATE TABLE Vehiculos_Terminados (
     id_vehiculo int identity (1,1) NOT NULL,
     nombre varchar (50) NOT NULL,
     color varchar (50) NOT NULL,
-    tipoV varchar (50) NOT NULL,
-    precio money  NOT NULL,
-    CONSTRAINT Vehiculos_Terminados_pk PRIMARY KEY  (id_vehiculo)
-)
+    tipoV_id int NOT NULL, -- Cambio a tipoV_id para referenciar la nueva tabla de tipos de vehículos
+    precio money NOT NULL,
+    CONSTRAINT Vehiculos_Terminados_pk PRIMARY KEY (id_vehiculo),
+    CONSTRAINT fk_vehiculos_tipoV FOREIGN KEY (tipoV_id) REFERENCES Tipos_Vehiculos(id_tipoV) -- Clave foránea hacia Tipos_Vehiculos
+);
+
 
 CREATE TABLE Localidades (
     Id_localidad int identity (1,1) NOT NULL,
@@ -100,6 +113,7 @@ CREATE TABLE Vendedores (
 )
 
 
+
 CREATE TABLE Orden_Pedido (
     nro_orden int identity (1,1) NOT NULL,
     fecha_entrega smalldatetime  NOT NULL,
@@ -118,11 +132,13 @@ CREATE TABLE Facturas (
     nroFac int identity (1,1) NOT NULL,
     fecha smalldatetime  NOT NULL,
     id_forma_pago int  NOT NULL,
+	id_forma_envio int NOT NULL,
     id_cliente int  NOT NULL,
     nro_orden int  NOT NULL,
     Id_vendedor int  NOT NULL,
     CONSTRAINT Facturas_pk PRIMARY KEY  (nroFac),
 	CONSTRAINT fk_facturas_clientes FOREIGN KEY (id_cliente) references clientes (id_cliente),
+	CONSTRAINT fk_id_forma_envio FOREIGN KEY (id_forma_envio) references Formas_envio (id_forma_envio),
 	CONSTRAINT fk_facturas_oden FOREIGN KEY (nro_orden) references orden_pedido (nro_orden),
 	CONSTRAINT fk_facturas_vendedores FOREIGN KEY (id_vendedor) references vendedores (id_vendedor),
 	CONSTRAINT fk_facturas_forma_pago FOREIGN KEY (id_forma_pago) references formas_pago (id_forma_pago)
@@ -153,5 +169,24 @@ CREATE TABLE DetalleFactura (
 	CONSTRAINT fk_detalle_auto_plan FOREIGN KEY (id_autoplan) references autoPlan (id_autoplan),
 	CONSTRAINT fk_detalle_facturas FOREIGN KEY (nrofac) references facturas (nrofac)
 )
+
+CREATE TABLE Tipos_contactos (
+	id_tipo_contacto int identity(1,1) not null,
+	descripcion varchar(80) not null
+	CONSTRAINT pk_id_tipo_contacto PRIMARY KEY(id_tipo_contacto)
+)
+
+CREATE TABLE Contactos (
+	id_contacto int identity(1,1),
+	contacto varchar(50) null,
+	id_cliente int null,
+	id_vendedor int null, 
+	id_tipo_contacto int null,
+	CONSTRAINT pk_id_contacto PRIMARY KEY  (id_contacto),
+	CONSTRAINT fk_id_cliente FOREIGN KEY (id_cliente) references clientes (id_cliente),
+	CONSTRAINT fk_id_vendedor FOREIGN KEY (id_vendedor) references vendedores (id_vendedor),
+	CONSTRAINT fk_id_tipo_contacto FOREIGN KEY (id_tipo_contacto) references tipos_contactos (id_tipo_contacto)
+);
+
 
 
